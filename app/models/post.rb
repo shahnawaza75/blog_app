38934234b60cc2a81a24f7607601b39339956027
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-09-17 13:14:05 +0530

    Fixed linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    class Post < ApplicationRecord
-      belongs_to :author, class_name: 'User'
-      has_many :comments
-      has_many :likes
-  
-      after_save :update_posts_counter
-  
-      def update_posts_counter
-        author.increment!(:post_counter)
-      end
-  
-      def recent_five_comments
-        comments.order('created_at Desc').limit(5)
-      end
+  class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+
+    after_save :update_posts_counter
+
+    def update_posts_counter
+      author.increment!(:post_counter)
+    end
+
+    def recent_five_comments
+      comments.order('created_at Desc').limit(5)
     end
   end
+end
