08cee3097bdf120838ff5ca1bfd10f7a2bbd3361
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-10-01 15:33:53 +0530

    Fixed issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,28 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id'
-  has_many :likes, foreign_key: 'post_id'
-
   after_save :update_posts_counter
+  before_destroy :update_posts_counter_on_destroy
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  private
+  validates :title, presence: true, length: { in: 4..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def last_five_comments
+    comments.includes(:post).order(created_at: :desc).limit(5)
+  end
+
+  def update_posts_counter_on_destroy
+    author.update_columns('posts_counter' => author.posts_counter - 1)
   end
 end
